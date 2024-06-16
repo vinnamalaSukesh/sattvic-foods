7f2b28a2969c0896ca7a114ff51bf5f2cafb10c1
sukesh reddy <sukeshreddy319@gmail.com>
2024-06-16 17:50:59 +0530

    restaurant

--- restaurant/wsgi.py
+++ restaurant/wsgi.py
@@ -2,4 +2,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant.settings')
 application = get_wsgi_application()
-app = application
