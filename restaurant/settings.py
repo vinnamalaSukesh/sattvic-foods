174137a4c9180faab2fb8eec3bc76bd71d5e30d2
sukesh reddy <sukeshreddy319@gmail.com>
2024-06-28 20:26:27 +0530

    restaurant

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'restaurant.urls'
 
